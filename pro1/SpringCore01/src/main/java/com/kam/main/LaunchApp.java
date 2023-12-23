dd10bf0c45695385f8c2ff6a2e43090fd25aa85a
KamilOgonowski <kml.ogonowski@gmail.com>
2023-12-23 22:32:21 +0100

    check whether spring created objects (beans)

--- pro1/SpringCore01/src/main/java/com/kam/main/LaunchApp.java
+++ pro1/SpringCore01/src/main/java/com/kam/main/LaunchApp.java
@@ -1,8 +1,16 @@
 package com.kam.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class LaunchApp {
 
 	public static void main(String[] args) {
+		
+		//application context or beanfactory to activate Spring (similar as it was with hibernate)
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml"); // to activate a container -> ref variable set as a parent of class of the object
+		
 	
 
 	}
